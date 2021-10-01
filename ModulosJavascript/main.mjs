f2015514d1fd2ec26c4418280ce86842f1aeadd0
hbauhvagner <hbauhvagner@gmail.com>
2021-09-30 23:45:29 -0300

    Adicionando modulo de Modulos no JavaScript

---
+++ ModulosJavascript/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Vagner", 30));
+console.log(mostraCidade("Vagner", "Ocidental"));
+console.log(mostraHobby("Vagner", "Jogar Dota 2"));
